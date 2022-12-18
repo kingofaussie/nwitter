264db9371f0ebacb1e707d49bc04ad903fe4e7fa
zxczxc123 <108798401+kingofaussie@users.noreply.github.com>
2022-12-18 17:29:50 +0900

    3.1 posting

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -16,3 +17,5 @@ const app = initializeApp(firebaseConfig);
 export default app;
 
 export const authService = getAuth(app);
+
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from 'fbase';
+import { collection, addDoc } from 'firebase/firestore';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const[nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, "nweets"), {
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const { 
+      target: {value},
+    } = event;
+    setNweet(value); 
+  };
+  return (
+      <div>
+          <form onSubmit={onSubmit}>
+              <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} /> 
+              <input type="submit" value="POST" />
+          </form>
+      </div>
+  );
+};
 export default Home;
+
+
+// input 태그의 maxLength 는 글자수 제한
