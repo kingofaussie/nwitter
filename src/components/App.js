dcf8740b5e3d812f9cdc493face1bca69322f459
zxczxc123 <108798401+kingofaussie@users.noreply.github.com>
2022-12-19 21:31:14 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,41 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function App() {
   const [init, setInit] = useState(false);
+<<<<<<< Updated upstream
+=======
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+>>>>>>> Stashed changes
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if(user) {
+<<<<<<< Updated upstream
         setUserObj(user);
       } 
+=======
+        setIsLoggedIn(true);
+        setUserObj(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+>>>>>>> Stashed changes
       setInit(true)
     })
   }, []);
   return (
   <>
+<<<<<<< Updated upstream
     {init ? (
     <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/>
     ) : (
       'Initailizing...'
       )}
    
+=======
+    {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>: 'Initailizing...'}
+    <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+>>>>>>> Stashed changes
   </>
   );
 }
